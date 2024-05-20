ef48d46b333d7ea70c06e8b46e72e807ae357fea
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-05-20 15:01:06 +0700

    memasukan for

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -18,3 +18,9 @@ class ibu {
 void ibu::tambahAnak(anak* pAnak){
     daftar_anak.push_bacl(pAnak);
 }
+void ibu::cetakAnak(){
+    cout << "Daftar Anak dari ibu"\"" << this->nama << "|":\n";
+    forr(auto& a : daftar_anak){
+        cout << a->nama << "\n";
+    }
+}
