caf8a6920ba90ff2f536db1e15a6bca0113129a9
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-05-20 15:19:14 +0700

    memasukan header

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -19,8 +19,10 @@ void ibu::tambahAnak(anak* pAnak){
     daftar_anak.push_bacl(pAnak);
 }
 void ibu::cetakAnak(){
-    cout << "Daftar Anak dari ibu"\"" << this->nama << "|":\n";
-    forr(auto& a : daftar_anak){
+    cout << "Daftar Anak dari ibu \"" << this->nama << "\":\n";
+    for(auto& a : daftar_anak){
         cout << a->nama << "\n";
     }
+    cout << endl;
 }
+#endif
